9ed50e943095c4410357bf66e6030203f6031fe6
Dipanjan Maji <mdipanjan2002@gmail.com>
2026-04-08 08:06:35 +0000

    fleet, nav2_planner_plugins, diagnostics

---
+++ fleet_management/include/fleet_management/fleet_manger.hpp
@@ -0,0 +1,11 @@
+/*
+Fleet management package essentially acheives the below:
+*Initializes Open RMF
+*Register robot
+*Send Nav2 goal
+*Check robot state
+*Feed position back to Open RMF
+*/
+
+#pragma once
+
---
+++ fleet_management/src/fleet_manager.cpp
@@ -0,0 +1,1 @@
+#include "fleet_management/fleet_manager.hpp"
